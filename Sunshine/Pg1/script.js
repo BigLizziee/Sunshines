5048de3104226415caff07791f848a445bd90de5
BigLizziee <yokoasuka29@gmail.com>
2024-12-10 16:18:59 -0300

    Add files via upload

--- Sunshine/Pg1/script.js
+++ Sunshine/Pg1/script.js
@@ -19,18 +19,36 @@ toggleButton.addEventListener('click', () => {
     const links = document.querySelectorAll('nav ul li a');
     links.forEach(link => link.classList.toggle('dark-mode'));
 
-    // Mudar as imagens dependendo do modo
+    
     if (body.classList.contains('dark-mode')) {
-        logo.src = '../img/dark1.png'; // Caminho para a imagem do logo no modo escuro
-        image1.src = '../img/dark2.png'; // Caminho para a imagem 1 no modo escuro
-        image2.src = '../img/dark3.png'; // Caminho para a imagem 2 no modo escuro
-        image3.src = '../img/dark4.png'; // Caminho para a imagem 3 no modo escuro
-        image4.src = '../img/dark5.png'; // Caminho para a imagem 4 no modo escuro
+        logo.src = '../img/dark1.png'; 
+        image1.src = '../img/dark2.png'; 
+        image2.src = '../img/dark3.png'; 
+        image3.src = '../img/dark4.png'; 
+        image4.src = '../img/dark5.png'; 
     } else {
-        logo.src = '../img/Sunshine.png'; // Caminho para a imagem do logo no modo claro
-        image1.src = '../img/pg01.png'; // Caminho para a imagem 1 no modo claro
-        image2.src = '../img/4.png'; // Caminho para a imagem 2 no modo claro
-        image3.src = '../img/5.png'; // Caminho para a imagem 3 no modo claro
-        image4.src = '../img/7.png'; // Caminho para a imagem 4 no modo claro
+        logo.src = '../img/Sunshine.png'; 
+        image1.src = '../img/pg01.png'; 
+        image2.src = '../img/4.png';
+        image3.src = '../img/5.png'; 
+        image4.src = '../img/7.png'; 
     }
 });
+const button = document.getElementById('dark');
+const icon = document.getElementById('icon');
+
+// Alterar ícone ao clicar no botão
+button.addEventListener('click', () => {
+    if (icon.classList.contains('fa-sun')) {
+        icon.classList.remove('fa-sun'); // Remover ícone do sol
+        icon.classList.add('fa-moon'); // Adicionar ícone da lua
+        button.style.backgroundColor = '#333'; // Alterar a cor do fundo para um tom escuro (lua)
+    } else {
+        icon.classList.remove('fa-moon'); // Remover ícone da lua
+        icon.classList.add('fa-sun'); // Adicionar ícone do sol
+        button.style.backgroundColor = '#ff66b2'; // Voltar a cor de fundo para rosa (sol)
+    }
+});
+
+
+
